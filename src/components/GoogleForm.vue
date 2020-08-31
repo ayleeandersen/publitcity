79fcb2fbf65de267488956f186bb03f5e2a64666
Aylee <ayleeandersen@gmail.com>
2020-08-31 17:17:02 -0600

    Made it mobile-friendly.

--- src/components/GoogleForm.vue
+++ src/components/GoogleForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hello">
     <h1>Welcome!</h1>
-    <h2>Register to join Publicity here: <a href="https://myinstitute.churchofjesuschrist.org/classes/16878745">https://myinstitute.churchofjesuschrist.org/classes/16878745</a></h2>
-    <iframe :src="form" width="640" height="1288" frameborder="0" marginheight="0" marginwidth="0" scrolling="no">Loading…</iframe>
-    <h2>To learn more about LDSSA, visit: <a href="https://instituteinlogan.org/ldssa/">https://instituteinlogan.org/ldssa/</a></h2>
+    <h2>Register to join Publicity <a href="https://myinstitute.churchofjesuschrist.org/classes/16878745">here</a></h2>
+    <h2>To learn more about LDSSA, visit <a href="https://instituteinlogan.org/ldssa/">instituteinlogan.org/ldssa</a></h2>
+    <iframe :src="form" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   </div>
 </template>
 
@@ -30,6 +30,11 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: rgb(3, 169, 244);
+}
+iframe {
+  width: 100%;
+  max-width: 640px;
+  height: 1500px;
 }
 </style>
